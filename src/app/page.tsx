c7bcf46bc0bdc4828f661ae541c3f306a1cb3922
Rodrigo Meliande <rodrigomeliande@msn.com>
2023-05-22 12:12:25 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,15 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 export default function Home() {
-  const [test, setTest] = useState(0)
+  const [test, setTest] = useState(4)
 
   const handleValueChange = () => {
-    if (test > 8) {
-      setTest(0)
+    if (test > 16) {
+      setTest(4)
+      console.log(test)
     } else {
       setTest(test + 1)
+      console.log(test)
     }
   }
 
@@ -19,14 +21,29 @@ export default function Home() {
       <h1 className="text-white text-center font-bold text-2xl pt-5 col-span-8 pb-3">
         HAY DAY HOJE??
       </h1>
-      <button className="bg-green-400 rounded-md p-10 flex items-center justify-center col-start-4 col-span-1 hover:bg-green-500">
+      <button className="bg-green-400 rounded-md p-10 flex items-center justify-center col-span-1 col-start-4 hover:bg-green-500">
         <Link href="https://hayday.com" target="_blank">
           Sim! 😃
         </Link>
       </button>
+      {test >= 5 && <div className="col-span-1"></div>}
+      {test >= 6 && <div className="col-span-1"></div>}
+      {test >= 7 && <div className="col-span-1"></div>}
+      {test >= 8 && <div className="col-span-1"></div>}
+      {test >= 9 && <div className="col-span-1"></div>}
+      {test >= 10 && <div className="col-span-1"></div>}
+      {test >= 11 && <div className="col-span-1"></div>}
+      {test >= 12 && <div className="col-span-1"></div>}
+      {test >= 13 && <div className="col-span-1"></div>}
+      {test >= 14 && <div className="col-span-1"></div>}
+      {test >= 15 && <div className="col-span-1"></div>}
+      {test >= 16 && <div className="col-span-1"></div>}
+      {test >= 17 && <div className="col-span-1"></div>}
+      {test >= 18 && <div className="col-span-1"></div>}
+      {test >= 19 && <div className="col-span-1"></div>}
       <button
         onClick={() => handleValueChange()}
-        className={`bg-red-400 rounded-md p-10 flex items-center justify-center col-span-1 col-start-${test} hover:bg-red-500`}
+        className={`bg-red-400 rounded-md p-10 flex items-center justify-center col-span-1 hover:bg-red-500`}
       >
         Não 😐
       </button>
